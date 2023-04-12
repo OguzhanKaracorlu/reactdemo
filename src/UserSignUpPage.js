9ac4294464c5ee817b75c8281ed00e1bc7ab1c40
oguzhan.karacorlu <oguzhankaracorlu@gmail.com>
2023-04-12 11:54:06 +0300

    Login Screen add pending transactions

--- src/UserSignUpPage.js
+++ src/UserSignUpPage.js
@@ -9,10 +9,11 @@ class UserSignUpPage extends React.Component {
      */
     state = {
         username: null,
-  /*      agreedClicked: false,*/
+        /*      agreedClicked: false,*/
         displayName: null,
         password: null,
-        passwordRepeat: null
+        passwordRepeat: null,
+        pendingApiCall: false
     };
 
     /**
@@ -74,45 +75,57 @@ class UserSignUpPage extends React.Component {
             displayName,
             password
         };
+        this.setState({pendingApiCall: true});
         axios.post('/api/users', body)
+            .then((response) => {
+                this.setState({pendingApiCall: false});
+            }).catch(error => {
+            this.setState({pendingApiCall: false});
+        })
     };
 
     render() {
         return (
             <div className="container">
-            <form>
-                <h1 className="text-center">Sign Up</h1>
-                <div className="form-group">
-                    <label>Username</label>
-                    <input className="form-control" name="username" onChange={this.onChange}/>
-                </div>
+                <form>
+                    <h1 className="text-center">Sign Up</h1>
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input className="form-control" name="username" onChange={this.onChange}/>
+                    </div>
 
-                <div className="form-group">
-                    <label>Display Name</label>
-                    <input className="form-control"  name="displayName" onChange={this.onChange}/>
-                </div>
+                    <div className="form-group">
+                        <label>Display Name</label>
+                        <input className="form-control" name="displayName" onChange={this.onChange}/>
+                    </div>
 
-                <div className="form-group" >
-                    <label>Password</label>
-                    <input className="form-control"  name="password" onChange={this.onChange} type="password"/>
-                </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input className="form-control" name="password" onChange={this.onChange} type="password"/>
+                    </div>
 
-                <div className="form-group">
-                    <label>Password Repeat</label>
-                    <input className="form-control"   name="passwordRepeat" onChange={this.onChange} type="password"/>
-                </div>
-
-
-                {/*<input type="checkbox" onChange={this.onChange}/> Agreed*/}
+                    <div className="form-group">
+                        <label>Password Repeat</label>
+                        <input className="form-control" name="passwordRepeat" onChange={this.onChange} type="password"/>
+                    </div>
 
 
-               <div className="text-center">
-                   <button className="btn btn-primary"/*disabled={!this.state.agreedClicked}*/ onClick={this.onClickSignUp} > Sign Up</button>
-               </div>
+                    {/*<input type="checkbox" onChange={this.onChange}/> Agreed*/}
 
-            </form>
+
+                    <div className="text-center">
+                        <button className="btn btn-primary"
+                            /*disabled={!this.state.agreedClicked}*/
+                                disabled={this.state.pendingApiCall}
+                                onClick={this.onClickSignUp}>
+                            {this.state.pendingApiCall && <span className="spinner-border spinner-border-sm"> </span>} Sign Up
+                    </button>
+            </div>
+
+    </form>
     </div>
-        );
+    )
+        ;
     }
 }
 
